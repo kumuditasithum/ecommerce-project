30cc1cff97758c2a35f4c6a0901448706d836738
Kumuditha Sithum <kumupro16@gmail.com>
2026-07-01 10:19:14 +0530

    Genarate HTML for the cart items

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Header } from "../components/Header";
+import { formatMoney } from '../utils/money';
 import "./HomePage.css";
 import checkMark from "../assets/images/icons/checkmark.png";
 
@@ -46,7 +47,7 @@ export function HomePage({cart}) {
                   <div className="product-rating-count link-primary">{product.rating.count}</div>
                 </div>
 
-                <div className="product-price">${(product.priceCents / 100).toFixed(2)}</div>
+                <div className="product-price">{formatMoney(product.priceCents)}</div>
 
                 <div className="product-quantity-container">
                   <select>
