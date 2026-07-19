2db6b1dedf9057f50a0b83044fb5b01c1cb33bf1
Kumuditha Sithum <kumupro16@gmail.com>
2026-07-19 10:04:01 +0530

    Make pdate button work

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -1,13 +1,39 @@
 
 import axios from "axios";
 import { formatMoney } from "../../utils/money";
+import { useState } from "react";
 
 export function CartItemDetails({cartItem, loadCart}) {
+ const [quantity, setQuantity] = useState(cartItem.quantity);
+ const [update, setUpdate] = useState(false);
 
  const deleteCartItem = async()=>{
   await axios.delete(`/api/cart-items/${cartItem.productId}`);
   await loadCart();
+ }
+ const updateItem = async()=>{
+  if(update){
+    await axios.put(`/api/cart-items/${cartItem.productId}`,{
+    quantity: Number(quantity)
+    });
+    setUpdate(false);
+    await loadCart();
+  }else{
+    setUpdate(true);
+  }
+ }
+ function changeQuantity(event){
+  setQuantity(event.target.value);
  } 
+ function inputKey(event){
+  if (event.key == 'Enter'){
+    updateItem();
+  } else  if(event.key == 'Escape'){
+    setQuantity(cartItem.quantity);
+    setUpdate(false);
+  }
+ }
+
   return (
     <>
       <img className="product-image" src={cartItem.product.image} />
@@ -20,9 +46,14 @@ export function CartItemDetails({cartItem, loadCart}) {
         <div className="product-quantity">
           <span>
             Quantity:{" "}
-            <span className="quantity-label">{cartItem.quantity}</span>
+            <span className="quantity-label">
+              { update ? <input type="text" className="quantity-input" value={quantity}
+                onChange={changeQuantity}
+                onKeyDown={inputKey}/>
+                      : cartItem.quantity}
+            </span>
           </span>
-          <span className="update-quantity-link link-primary">Update</span>
+          <span className="update-quantity-link link-primary" onClick={updateItem}>Update</span>
           <span className="delete-quantity-link link-primary" onClick={deleteCartItem}>Delete</span>
         </div>
       </div>
